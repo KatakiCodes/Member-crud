9a59859b021315d903d896bf6800ec6d67a0af24
katakicodes <katakicode001@gmail.com>
2025-07-10 13:04:16 +0100

    fix: Added Enum to set gender and comparison method to member model

--- CleanArch/CleanArch.Domain/Entities/Member.cs
+++ CleanArch/CleanArch.Domain/Entities/Member.cs
@@ -1,33 +1,42 @@
-﻿using CleanArch.Domain.Validation;
+﻿using CleanArch.Domain.Enums;
+using CleanArch.Domain.Validation;
+using System.Text.RegularExpressions;
 
 namespace CleanArch.Domain.Entities
 {
-    public sealed class Member : Entity
+    public sealed class Member : Entity, IEquatable<Member>
     {
-        public Member(string firstName, string lastName, string gender, string email, bool isActive)
+        public Member(string firstName, string lastName, EGender gender, string email, bool isActive)
         {
             ValidateDomain(firstName, lastName, gender,email,isActive);
         }
-        public Member(int id, string firstName, string lastName, string gender, string email, bool isActive)
+        public Member(int id, string firstName, string lastName, EGender gender, string email, bool isActive)
         {
             DomainValidation.When(id < 0, "Invalid Id value");
             base.Id = id;
             ValidateDomain(firstName, lastName, gender, email, isActive);
         }
 
-        public string? FirstName { get; private set; }
-        public string? LastName { get; private set; }
-        public string? Gender { get; private set; }
-        public string? Email { get; private set; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public EGender Gender { get; private set; }
+        public string Email { get; private set; }
         public bool IsActive { get; private set; }
 
-        private void ValidateDomain(string firstName, string lastName, string gender, string email, bool isActive)
+        public bool Equals(Member? other)
+        {
+            if(other is null) 
+                return false;
+            return (other.Id == this.Id && other.FirstName.Equals(other.FirstName, StringComparison.OrdinalIgnoreCase));
+        }
+
+        private void ValidateDomain(string firstName, string lastName, EGender gender, string email, bool isActive)
         {
             DomainValidation.When(string.IsNullOrEmpty(firstName), "The first name cannot be null or empty");
-            DomainValidation.When((firstName.Length < 3), "The first name must contains over 3 characters");
+            DomainValidation.When((firstName.Length < 3), "The first name must contain at the least 3 characters");
             DomainValidation.When(string.IsNullOrEmpty(lastName), "The last name cannot be null or empty!");
-            DomainValidation.When(string.IsNullOrEmpty(gender), "The gender cannot be null or empty!");
-            DomainValidation.When((string.IsNullOrEmpty(email) || !email.Contains('@')), "Insert a valid email!");
+            DomainValidation.When(string.IsNullOrEmpty(gender.ToString()), "The gender cannot be null or empty!");
+            DomainValidation.When(!Regex.IsMatch(email, @"^[^@\s]+@[^@\s]+\.[^@\s]+$"), "Insert a valid email!");
 
             FirstName = firstName;
             LastName = lastName;
@@ -35,5 +44,6 @@ namespace CleanArch.Domain.Entities
             Email = email;
             IsActive = isActive;
         }
+        
     }
 }
---
+++ CleanArch/CleanArch.Domain/Enums/EGender.cs
@@ -0,0 +1,9 @@
+﻿namespace CleanArch.Domain.Enums
+{
+    public enum EGender
+    {
+        Male = 1,
+        Female = 2,
+        Other = 3
+    }
+}
