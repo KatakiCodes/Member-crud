07d0c64e60005c3d379b9408f1e818eaba12ebc3
katakicodes <katakicode001@gmail.com>
2025-07-10 11:54:54 +0100

    fix: Error message on firtName validation

--- CleanArch/CleanArch.Domain/Entities/Member.cs
+++ CleanArch/CleanArch.Domain/Entities/Member.cs
@@ -23,7 +23,8 @@ namespace CleanArch.Domain.Entities
 
         private void ValidateDomain(string firstName, string lastName, string gender, string email, bool isActive)
         {
-            DomainValidation.When((string.IsNullOrEmpty(firstName) || firstName.Length < 3), "The first name cannot be null or empty");
+            DomainValidation.When(string.IsNullOrEmpty(firstName), "The first name cannot be null or empty");
+            DomainValidation.When((firstName.Length < 3), "The first name must contains over 3 characters");
             DomainValidation.When(string.IsNullOrEmpty(lastName), "The last name cannot be null or empty!");
             DomainValidation.When(string.IsNullOrEmpty(gender), "The gender cannot be null or empty!");
             DomainValidation.When((string.IsNullOrEmpty(email) || !email.Contains('@')), "Insert a valid email!");
