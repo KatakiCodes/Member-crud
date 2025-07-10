aeb27baa89b8b98b7fe9e5d0b142fedcb41317d6
katakicodes <katakicode001@gmail.com>
2025-07-10 11:12:10 +0100

    feat: Created Member contract repository

---
+++ CleanArch/CleanArch.Domain/Abstractions/IMemberRepository.cs
@@ -0,0 +1,13 @@
+﻿using CleanArch.Domain.Entities;
+
+namespace CleanArch.Domain.Abstractions
+{
+    public interface IMemberRepository
+    {
+        public Task<IEnumerable<Member>>GetMembers();
+        public Task<Member> GetMemberById(int id);
+        public Task<Member> CreateMember(Member member);
+        public Task<Member> UpdateMember(Member member);
+        public Task DeleteMember(Member member);
+    }
+}
